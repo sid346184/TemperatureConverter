036ec24f81361250517cbc24463de9c07d6e3444
Siddhant Gupta <71984091+sid346184@users.noreply.github.com>
2023-10-06 17:48:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+let celcius = document.querySelector('#celcius');
+let fahrenheit = document.querySelector('#fahrenheit');
+let kelvin = document.querySelector('#kelvin');
+
+function roundNumber(number) {
+    return Math.round(number * 100) / 100;
+}
+
+function noNumber() {
+    celcius.value = "";
+    fahrenheit.value = "";
+    kelvin.value = "";
+}
+
+celcius.addEventListener('input', function () {
+    let cTemp = parseFloat(celcius.value);
+    if (!isNaN(cTemp)) {
+        let fTemp = (cTemp * (9 / 5)) + 32;
+        let kTemp = cTemp + 273.15;
+        fahrenheit.value = roundNumber(fTemp);
+        kelvin.value = roundNumber(kTemp);
+    } else {
+        noNumber();
+    }
+});
+
+fahrenheit.addEventListener('input', function () {
+    let fTemp = parseFloat(fahrenheit.value);
+    if (!isNaN(fTemp)) {
+        let cTemp = (fTemp - 32) * (5 / 9);
+        let kTemp = (fTemp - 32) * (5 / 9) + 273.15;
+        celcius.value = roundNumber(cTemp);
+        kelvin.value = roundNumber(kTemp);
+    } else {
+        noNumber();
+    }
+});
+
+kelvin.addEventListener('input', function () {
+    let kTemp = parseFloat(kelvin.value);
+    if (!isNaN(kTemp)) {
+        let fTemp = (kTemp - 273.15) * (9 / 5) + 32;
+        let cTemp = kTemp - 273.15;
+        fahrenheit.value = roundNumber(fTemp);
+        celcius.value = roundNumber(cTemp);
+    } else {
+        noNumber();
+    }
+});
+
+button.addEventListener('click',()=>{
+    celcius.value="";
+    kelvin.value="";
+    fahrenheit.value="";
+})
