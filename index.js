d5305b46f33d0a59f41d0d36899a0a621252bdb8
Siddhant Gupta <71984091+sid346184@users.noreply.github.com>
2023-10-06 23:21:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,72 +2,79 @@ let celcius = document.querySelector('#celcius');
 let fahrenheit = document.querySelector('#fahrenheit');
 let kelvin = document.querySelector('#kelvin');
 let button = document.querySelector('#button');
-let minus = document.querySelector('#minus'); 
+let minus = document.querySelector('#minus');
+
 function roundNumber(number) {
-    return Math.round(number * 100) / 100;
+  return Math.round(number * 100) / 100;
 }
 
 function noNumber() {
-    celcius.value = "";
-    fahrenheit.value = "";
-    kelvin.value = "";
+  celcius.value = "";
+  fahrenheit.value = "";
+  kelvin.value = "";
 }
 
 let minusClickCount = 0;
+let hasInput = false;
 
 minus.addEventListener('click', () => {
-  minusClickCount++;
-  if (minusClickCount % 2 === 0) {
-    celcius.value = celcius.value.replace('-', '');
-    kelvin.value = kelvin.value.replace('-', '');
-    fahrenheit.value = fahrenheit.value.replace('-', '');
-  } else {
-    celcius.value = "-" + celcius.value;
-    kelvin.value = "-" + kelvin.value;
-    fahrenheit.value = "-" + fahrenheit.value;
+  if (!hasInput) {
+    minusClickCount++;
+    if (minusClickCount % 2 === 0) {
+      celcius.value = celcius.value.replace('-', '');
+      kelvin.value = kelvin.value.replace('-', '');
+      fahrenheit.value = fahrenheit.value.replace('-', '');
+    } else {
+      celcius.value = "-" + celcius.value;
+      kelvin.value = "-" + kelvin.value;
+      fahrenheit.value = "-" + fahrenheit.value;
+    }
   }
 });
 
 celcius.addEventListener('input', function () {
-    let cTemp = parseFloat(celcius.value);
-    if (!isNaN(cTemp)) {
-        let fTemp = (cTemp * (9 / 5)) + 32;
-        let kTemp = cTemp + 273.15;
-        fahrenheit.value = roundNumber(fTemp);
-        kelvin.value = roundNumber(kTemp);
-    } else {
-        noNumber();
-    }
+  hasInput = true;
+  let cTemp = parseFloat(celcius.value);
+  if (!isNaN(cTemp)) {
+    let fTemp = (cTemp * (9 / 5)) + 32;
+    let kTemp = cTemp + 273.15;
+    fahrenheit.value = roundNumber(fTemp);
+    kelvin.value = roundNumber(kTemp);
+  } else {
+    noNumber();
+  }
 });
 
 fahrenheit.addEventListener('input', function () {
-    let fTemp = parseFloat(fahrenheit.value);
-    if (!isNaN(fTemp)) {
-        let cTemp = (fTemp - 32) * (5 / 9);
-        let kTemp = (fTemp - 32) * (5 / 9) + 273.15;
-        celcius.value = roundNumber(cTemp);
-        kelvin.value = roundNumber(kTemp);
-    } else {
-        noNumber();
-    }
+  hasInput = true;
+  let fTemp = parseFloat(fahrenheit.value);
+  if (!isNaN(fTemp)) {
+    let cTemp = (fTemp - 32) * (5 / 9);
+    let kTemp = (fTemp - 32) * (5 / 9) + 273.15;
+    celcius.value = roundNumber(cTemp);
+    kelvin.value = roundNumber(kTemp);
+  } else {
+    noNumber();
+  }
 });
 
 kelvin.addEventListener('input', function () {
-    let kTemp = parseFloat(kelvin.value);
-    if (!isNaN(kTemp)) {
-        let fTemp = (kTemp - 273.15) * (9 / 5) + 32;
-        let cTemp = kTemp - 273.15;
-        fahrenheit.value = roundNumber(fTemp);
-        celcius.value = roundNumber(cTemp);
-    } else {
-        noNumber();
-    }
+  hasInput = true;
+  let kTemp = parseFloat(kelvin.value);
+  if (!isNaN(kTemp)) {
+    let fTemp = (kTemp - 273.15) * (9 / 5) + 32;
+    let cTemp = kTemp - 273.15;
+    fahrenheit.value = roundNumber(fTemp);
+    celcius.value = roundNumber(cTemp);
+  } else {
+    noNumber();
+  }
 });
 
-button.addEventListener('click',()=>{
-    celcius.value="";
-    kelvin.value="";
-    fahrenheit.value="";
-    minusClickCount = 0;
-})
-
+button.addEventListener('click', () => {
+  celcius.value = "";
+  kelvin.value = "";
+  fahrenheit.value = "";
+  minusClickCount = 0;
+  hasInput = false;
+});
